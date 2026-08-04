526bb713d2f2cedff2892b502b9002fba1d7b7b5
Emanuele Molinari <molinariemanuele22@gmail.com>
2026-08-05 01:09:05 +0200

    player da sistemare

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,38 @@
 #include <SFML/Graphics.hpp>
-#include <string>
-
-//percorso per raggiungere media, che viene copiata dentro build dallo script CMakeLists (riga script aggiunta con IA)
-std::string mediaPath = "../media/"; 
+#include "textures.hpp"
 
 int main()
 {
-	
-	
 	//recupero desktop
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window( sf::VideoMode( { desktop.size.x, desktop.size.y } ), "Space Invaders");
+	sf::RenderWindow window(sf::VideoMode({desktop.size.x, desktop.size.y}), "Space Invaders");
 	window.setFramerateLimit(60);
 	
-	//caricamento sfondo
-	sf::Texture sfondo;
-	sfondo.loadFromFile(mediaPath + "spacebackground.jpg");
-	sf::Sprite sfondo_sprite(sfondo); //serve per renderlo drawable
-
+	//caricamento sfondo da file hpp e dimensione corretta
+	sf::Texture background(spacebackground_jpg, spacebackground_jpg_len);
+	sf::Sprite background_sprite(background); //serve per renderlo drawable
+	
+	double background_scale_x = (static_cast<float>(desktop.size.x) / background.getSize().x); //facendo cast di 1 ottengo risultato float e non int
+	double background_scale_y = (static_cast<float>(desktop.size.y) / background.getSize().y);
+	background_sprite.setScale(sf::Vector2f(background_scale_x, background_scale_y));
+	
 	//bozza statica del giocatore
-	sf::RectangleShape player(sf::Vector2f(50, 100));
-	player.setPosition(sf::Vector2f(desktop.size.x / 2, desktop.size.y * 0.8)); 
+	sf::Texture player(player_png, player_png_len);
+	sf::Sprite player_sprite(player); //serve per renderlo drawable
+	//player_sprite.setPosition(sf::Vector2f(desktop.size.x / 2, desktop.size.y * 0.8)); 
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
 
 		//sfondo
 		window.clear();
-		window.draw(sfondo_sprite);
+		window.draw(background_sprite);
 
 		//giocatore
-		window.draw(player);
+		window.draw(player_sprite);
 
 		window.display();
 	}
