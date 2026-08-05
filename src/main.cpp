af6ef58695a71759d0330c114363c964e56bfc24
Emanuele Molinari <molinariemanuele22@gmail.com>
2026-08-05 17:07:30 +0200

    riscrittura tappa 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,82 @@
 #include <SFML/Graphics.hpp>
 #include "textures.hpp"
 
-int main()
-{
-	//recupero desktop
-	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window(sf::VideoMode({desktop.size.x, desktop.size.y}), "Space Invaders");
-	window.setFramerateLimit(60);
-	
-	//caricamento sfondo da file hpp e dimensione corretta
-	sf::Texture background(spacebackground_jpg, spacebackground_jpg_len);
-	sf::Sprite background_sprite(background); //serve per renderlo drawable
-	
-	double background_scale_x = (static_cast<float>(desktop.size.x) / background.getSize().x); //facendo cast di 1 ottengo risultato float e non int
-	double background_scale_y = (static_cast<float>(desktop.size.y) / background.getSize().y);
-	background_sprite.setScale(sf::Vector2f(background_scale_x, background_scale_y));
-	
-	//giocatore (statico)
-	sf::Texture player(player_png, player_png_len);
-	sf::Sprite player_sprite(player); //serve per renderlo drawable
+/*----------------------
+-------Game State-------
+----------------------*/
+struct State {
+    //risorse generali
+    sf::RenderWindow window;
+    sf::Texture background;
+    sf::Sprite background_sprite;
+    sf::Texture player;
+    sf::Sprite player_sprite;
 
-	//sposta origine al centro della texture
-    float player_centro_x = static_cast<float>(player.getSize().x) / 2.0;
-    float player_centro_y = static_cast<float>(player.getSize().y) / 2.0;
-    player_sprite.setOrigin(sf::Vector2f(player_centro_x, player_centro_y));
+    //caricamento texture e collegamento agli sprite prima del corpo del costruttore
+    State() :
+        background(spacebackground_jpg, spacebackground_jpg_len),
+        background_sprite(background),
+        player(player_png, player_png_len),
+        player_sprite(player)
+    {
+        //recupero desktop e creazione finestra
+        sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+        window.create(sf::VideoMode({desktop.size.x, desktop.size.y}), "Space Invaders");
+        window.setFramerateLimit(60);
+        
+        //sfondo di dimensione dello schermo
+        double background_scale_x = (static_cast<float>(desktop.size.x) / background.getSize().x); 
+        double background_scale_y = (static_cast<float>(desktop.size.y) / background.getSize().y);
+        background_sprite.setScale(sf::Vector2f(background_scale_x, background_scale_y));
 
-	player_sprite.setScale(sf::Vector2f(0.5, 0.5));
-	player_sprite.setPosition(sf::Vector2f(static_cast<float>(desktop.size.x) / 2, static_cast<float>(desktop.size.y) * 0.8)); 
+        //sposta origine di player al centro dello sprite
+        float player_centro_x = static_cast<float>(player.getSize().x) / 2.0;
+        float player_centro_y = static_cast<float>(player.getSize().y) / 2.0;
+        player_sprite.setOrigin(sf::Vector2f(player_centro_x, player_centro_y));
 
+        player_sprite.setScale(sf::Vector2f(0.5, 0.5));
+        player_sprite.setPosition(sf::Vector2f(static_cast<float>(desktop.size.x) / 2.0, static_cast<float>(desktop.size.y) * 0.8)); 
+    }
+};
 
+/*-----------------------------------------
+-----Funzioni callback gestione eventi-----
+-----------------------------------------*/
+void handle(const sf::Event::Closed &, State &gs) {
+    gs.window.close();
+}
 
+template <typename T>
+void handle(const T &, State &gs) { //eventi non gestiti esplicitamente
+    
+}
 
-	while (window.isOpen()) {
-		while (const std::optional event = window.pollEvent()) {
-			if (event->is<sf::Event::Closed>())
-				window.close();
-		}
+/*--------------
+------Grafica---
+--------------*/
+void doGraphics(State &gs) {
+    //sfondo
+    gs.window.clear();
+    gs.window.draw(gs.background_sprite);
 
-		//sfondo
-		window.clear();
-		window.draw(background_sprite);
+    //giocatore
+    gs.window.draw(gs.player_sprite);
 
-		//giocatore
-		window.draw(player_sprite);
+    gs.window.display();
+}
 
-		window.display();
-	}
+/*--------------
+---Main loop----
+--------------*/
+int main() {
+    State gs;
+    
+    while (gs.window.isOpen()) 
+    {
+        
+        gs.window.handleEvents([&](const auto &event)
+                               { handle(event, gs); });
+
+        doGraphics(gs);
+    }
 }
